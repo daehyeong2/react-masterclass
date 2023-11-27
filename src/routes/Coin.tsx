38710fcbd1326c2c42e054bb76de01e69afa5886
daehyeong2 <baconbacon1231@gmail.com>
2023-11-27 10:25:21 +0900

    3.5 ~ 5.9 ( REACT ROUTER V6 스킵 )

---
+++ src/routes/Coin.tsx
@@ -0,0 +1,249 @@
+import React, { useEffect, useState } from "react";
+import {
+  Link,
+  Route,
+  Switch,
+  useLocation,
+  useParams,
+  useRouteMatch,
+} from "react-router-dom";
+import styled from "styled-components";
+import Price from "./Price";
+import Chart from "./Chart";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.h3`
+  margin-top: 40px;
+  text-align: center;
+`;
+const Overview = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 480px;
+  border-radius: 15px;
+  height: 65px;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 0 30px;
+`;
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  padding: 13px 0;
+  span:first-child {
+    font-size: 13px;
+  }
+`;
+const Description = styled.p`
+  margin: 15px 0;
+`;
+const Back = styled.div`
+  margin-bottom: 10px;
+  a {
+    transition: color 0.1s ease-in-out;
+    &:hover {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
+const Tabs = styled.div`
+  margin-top: 20px;
+  display: flex;
+  height: 40px;
+  justify-content: space-between;
+  max-width: 480px;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+const Tab = styled.button<{ active: boolean }>`
+  width: 50%;
+  height: 100%;
+  border: 1px solid black;
+  transition: background-color 0.2s ease-in-out;
+  background-color: ${(props) => (props.active ? "#718093" : "#2f3640")};
+  a {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${(props) => props.theme.textColor};
+  }
+`;
+
+interface RouteParams {
+  coinId: string;
+}
+interface RouteState {
+  name: string;
+}
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+  error?: string;
+}
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
+function Coin() {
+  const { coinId } = useParams<RouteParams>();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState<InfoData>();
+  const [priceInfo, setPriceInfo] = useState<PriceData>();
+  const priceMatch = useRouteMatch("/:coinId/price");
+
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+      setLoading(false);
+    })();
+  }, [coinId]);
+
+  return (
+    <Container>
+      <Header>
+        <Title>
+          {info?.error
+            ? "404 Not Found."
+            : state?.name
+            ? state.name
+            : loading
+            ? "Loading..."
+            : info?.name}
+        </Title>
+      </Header>
+      <Back>
+        <Link to="/">&larr; Back</Link>
+      </Back>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <OverviewItem>
+              <span>RANK:</span>
+              <span>{info?.rank}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>SYMBOL:</span>
+              <span>{info?.symbol}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>OPEN SOURCE:</span>
+              <span>{info?.error ? "" : info?.open_source ? "Yes" : "No"}</span>
+            </OverviewItem>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <OverviewItem>
+              <h3>TOTAL SUPLY:</h3>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <h3>MAX SUPPLY:</h3>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          {info?.error ? null : (
+            <>
+              <Tabs>
+                <Tab active={priceMatch !== null}>
+                  <Link to={`/${coinId}/price`}>Price</Link>
+                </Tab>
+                <Tab active={priceMatch === null}>
+                  <Link to={`/${coinId}/chart`}>Chart</Link>
+                </Tab>
+              </Tabs>
+              <Switch>
+                <Route path="/:coinId/price">
+                  <Price />
+                </Route>
+                <Route path="/:coinId/chart">
+                  <Chart />
+                </Route>
+              </Switch>
+            </>
+          )}
+        </>
+      )}
+    </Container>
+  );
+}
+
+export default Coin;
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,137 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { faChartSimple } from "@fortawesome/free-solid-svg-icons";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useQuery } from "react-query";
+import { fetchCoins } from "../api";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinsList = styled.ul`
+  margin-top: 30px;
+`;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  border-radius: 15px;
+  position: relative;
+  a {
+    transition: color 0.15s ease-in-out;
+    display: flex;
+    padding: 20px;
+    align-items: center;
+    &:first-child:hover {
+      color: ${(props) => props.theme.accentColor};
+    }
+    &:last-child:hover {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+`;
+
+const Loader = styled.h3`
+  margin-top: 40px;
+  text-align: center;
+`;
+
+const CoinImage = styled.img`
+  margin-right: 10px;
+  width: 30px;
+  height: 30px;
+`;
+
+const ChartLink = styled.div`
+  position: absolute;
+  top: 21px;
+  right: 20px;
+  width: 30px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a {
+    padding: 0;
+  }
+`;
+
+interface ICoin {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>
+      </Header>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {data?.slice(0, 100).map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}/price`,
+                  state: {
+                    name: coin.name,
+                  },
+                }}
+              >
+                <CoinImage
+                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
+              <ChartLink>
+                <Link
+                  to={{
+                    pathname: `/${coin.id}/chart`,
+                    state: {
+                      name: coin.name,
+                    },
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={faChartSimple as IconProp}
+                  ></FontAwesomeIcon>
+                </Link>
+              </ChartLink>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
+    </Container>
+  );
+}
+
+export default Coins;
