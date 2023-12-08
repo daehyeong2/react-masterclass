9342ac048638e758f914ce55023234c6e2e6fd75
daehyeong2 <baconbacon1231@gmail.com>
2023-12-08 13:52:58 +0900

    6.17

--- src/atoms.ts
+++ src/atoms.ts
@@ -5,6 +5,12 @@ export interface IToDo {
   text: string;
   category: "TO_DO" | "DOING" | "DONE";
 }
+
+export const categoryState = atom({
+  key: "category",
+  default: "TO_DO",
+});
+
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
@@ -14,10 +20,7 @@ export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
     const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
   },
 });
